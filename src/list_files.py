dbc1e7d37fac2c0e213f5d3292b10759fef7bb2a
paulevalentine <62242813+paulevalentine@users.noreply.github.com>
2025-12-29 22:35:59 +0000

    First draft of a list files code to list what is in the issued folders or the external document folders.

---
+++ src/list_files.py
@@ -0,0 +1,30 @@
+import re
+from pathlib import Path
+
+path = Path("/Users/paulvalentine/Library/CloudStorage/Dropbox/Structensor/Jobs/Rowland Homes/6504 Roseacre/02 Internal Documents/02 Issued")
+output_file = Path("files.txt")
+
+# get a list of the directories in the designated path
+dirs = [p for p in path.iterdir() if p.is_dir()]
+
+# ensure that sort is based on the date reference
+def get_sort_key(folder_path):
+    match = re.search(r'(\d{8})', folder_path.name)
+    return match.group(1) if match else folder_path.name
+
+sorted_dirs = sorted(dirs, key=get_sort_key)
+
+with open(output_file, "w") as f:
+    for folder in sorted_dirs:
+        if folder.is_dir():
+            f.write(f"\nThe documents in {folder.name}:\n")
+            sub_path = path / folder.name
+            for sub_folder in sub_path.iterdir():
+                if sub_folder.is_dir() and sub_folder.name != ".DS_Store":
+                    f.write(f"\t{sub_folder.name}\n")
+                    for file in sub_folder.iterdir():
+                        f.write(f"\t \t{file.name}\n")
+            for file in folder.iterdir():
+                if file.is_file() and file.name != ".DS_Store":
+                    f.write(f"\t{file.name}\n")
+
